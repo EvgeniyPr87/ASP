2a3cb9540743afb89b4f1b7060ba30e7e0777cca
Evgeniy Pronyakin <ypronyakin@internet.ru>
2026-09-04 10:30:37 +0300

    Add Power and Fibonacci

---
+++ Blazor/Blazor/Components/Pages/Fibonacci.razor.cs
@@ -0,0 +1,28 @@
+﻿namespace Blazor.Components.Pages
+{
+    public partial class Fibonacci
+    {
+
+        int num;
+        int[] result;
+
+        public int[] FibCalculate(int num)
+        {
+
+        int[]fib = new int[num];
+            fib[0] = 0;
+            fib[1] = 1;
+
+            for(int i=2; i<num; i++)
+            {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+                return fib;
+
+        }
+        public void FibCreate()
+        {
+            result = FibCalculate(num);
+        }
+    }
+}
---
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Blazor.Components.Pages
+{
+    public partial class Power
+    {
+        double number = 1;
+        double degree = 1;
+        double result;
+        void CalculatePower()
+        {
+          result = Math.Pow(number, degree);
+        }
+    }
+}
