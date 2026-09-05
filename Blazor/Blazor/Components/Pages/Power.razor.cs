ad27e5817fc19c88c76b08884d774c4c43c488f7
Evgeniy Pronyakin <ypronyakin@internet.ru>
2026-09-05 08:34:41 +0300

    Fibonacci and Power DONE

--- Blazor/Blazor/Components/Pages/Power.razor.cs
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -3,12 +3,18 @@ namespace Blazor.Components.Pages
 {
     public partial class Power
     {
-        double number = 1;
-        double degree = 1;
+        double number = 0;
+        double degree = 0;
         double result;
         void CalculatePower()
         {
           result = Math.Pow(number, degree);
+        }
+        void Reset()
+        {
+            result = 0;
+            number = 0;
+            degree = 0;
         }
     }
 }
