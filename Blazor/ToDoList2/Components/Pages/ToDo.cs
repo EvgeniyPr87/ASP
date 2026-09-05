79053e9e030bd97f1aba98d31ef767004906df0b
Evgeniy Pronyakin <ypronyakin@internet.ru>
2026-09-05 21:47:36 +0300

    TaskList2

---
+++ Blazor/ToDoList2/Components/Pages/ToDo.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList2.Components.Pages
+{
+    public class ToDo
+    {
+        public string Description { get; set; }
+        public bool DONE { get; set; }
+        public override bool Equals(object? other)
+        {
+            return this.Description.Equals((other as ToDo).Description, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
